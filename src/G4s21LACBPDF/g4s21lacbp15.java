0f3c27235dd94173667e54d56ac0f6a8c5b33086
tanne <tanne@LAPTOP-F2TF4AUH>
2022-06-27 17:59:36 -0600

    LUIS ALBERTO CRUZ BARAHONA

--- src/G4s21LACBPDF/g4s21lacbp15.java
+++ src/G4s21LACBPDF/g4s21lacbp15.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package g4s21lacbp15;
+package G4s21LACBPDF;
 
 import com.itextpdf.io.font.FontConstants;
 import com.itextpdf.kernel.colors.Color;
